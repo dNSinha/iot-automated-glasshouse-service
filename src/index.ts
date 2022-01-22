7922fb0a87567b1d266c1b9f12fbfaf077eef34d
dNSinha <dnsinha33@gmail.com>
2022-01-22 13:32:46 +0100

    cors modificatoin to run swagger

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import { routes } from './api';
 import swaggerUi from 'swagger-ui-express';
+import cors  from 'cors';
 const yaml = require('yamljs');
 const swaggerDocument = yaml.load('./swagger.yaml');
 import { db } from './db';
@@ -12,11 +13,12 @@ const app = () => {
   app.use(bodyParser.json());
 
   db.init();
-  app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+  // app.use((req, res, next) => {
+  //   res.header("Access-Control-Allow-Origin", "*");
+  //   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  //   next();
+  // });
+  app.use(cors())
   app.use('/api', routes);
   if (process.env.ENABLE_SWAGGER === 'true')
     app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, { explorer: true }));
